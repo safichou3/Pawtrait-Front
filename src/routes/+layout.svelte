c6fc53a51dadf3eaf63566608620b5c91f08cb74
Houda Ousbane <ousbane.houda@gmail.com>
2023-10-02 11:33:19 +0200

    header footer mainpage

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script>
+    import "../app.css";
+    import Header from '../components/Header.svelte';
+	import Footer from '../components/Footer.svelte';
+  </script>
+
+<header>
+    <Header/>
+</header>
+
+<main>
+    <slot></slot>
+</main>
+
+<footer>
+<Footer/>
+</footer>
