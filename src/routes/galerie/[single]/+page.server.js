57be66aab780b19ef40caf63af567dbb98f71122
Safia <safia.medjahed@gmail.com>
2023-10-27 09:21:29 +0200

    feat: single image gallery style

--- src/routes/galerie/[single]/+page.server.js
+++ src/routes/galerie/[single]/+page.server.js
@@ -1,6 +1,6 @@
 /** @type {import('./$types').PageServerLoad} */
 export async function load({ fetch, params }) {
-    const response = await fetch("https://api.thecatapi.com/v1/images/search?api_key=live_c62EzoEgHNw9QRdu9JB4fU9JnZnBOpzV4SKpxaEo5iWNPAXIs7AM04Y4Vss31D6E/" + params.single);
+    const response = await fetch("https://api.thecatapi.com/v1/images/search?size=med&api_key=live_c62EzoEgHNw9QRdu9JB4fU9JnZnBOpzV4SKpxaEo5iWNPAXIs7AM04Y4Vss31D6E/" + params.single);
     const data = await response.json();
     // console.log(data)
     return {
