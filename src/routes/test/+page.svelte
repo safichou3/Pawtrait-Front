401a6a0b0be5e89a4569f8de676afa91b6d6044f
Safia <safia.medjahed@gmail.com>
2023-10-27 09:31:31 +0200

    feat: resolve conflicts

---
+++ src/routes/test/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+    /** @type {import('./$types').PageData} */
+    export let data = [];
+    console.log(data)
+</script>
+
+<a href="/api/create">Create</a>
+
+<!--<a href="{data.data.url}"><img src="{data.data.url}" alt=""></a>-->
+
+
+<ul>
+    {#each data.data as dataKey}
+        <li>
+<!--            <a href="{dataKey.url}">{dataKey.url}</a>-->
+            <img src="{dataKey.url}" alt="">
+        </li>
+    {/each}
+</ul>
