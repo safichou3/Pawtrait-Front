5180602943499b95333093cb97cb6306713394c2
Houda Ousbane <ousbane.houda@gmail.com>
2023-11-15 22:25:38 +0100

    connnexion reussite

--- src/routes/connexion/+page.server.js
+++ src/routes/connexion/+page.server.js
@@ -1,36 +1,48 @@
 /** @type {import('./$types').PageServerLoad} */
 export async function load() {
-    return {};
+    const initialData = {
+        username: '',
+        password: '',
+        rememberMe: false,
+    };
+
+    return {
+        data: initialData,
+    };
+};
+
+/** @type {import('./$types').Actions} */
+export const actions = {
+	login: async ({request}) => {
+        const data = await request.formData();        
+        const username = data.get('username');        
+        const password = data.get('password');
+        const rememberMe = data.get('rememberMe');
+
+        console.log(username);
+		try {
+            const response = await fetch('http://localhost:8080/api/authenticate', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    username,
+                    password,
+                    rememberMe,
+                }),                
+            });
+            if (response.ok) {
+                const responseData = await response.json();
+                console.log('Token de connexion:', responseData.id_token);
+            } else {
+                console.error('Échec de l\'authentification:', response.status, response.statusText, response.json);
+            }
+        } catch (error) {
+            console.error('Erreur lors de la requête:', error);
+        }
+}
 };
 
 
-/** @type {import('./$types').Actions} */
-export const actions = {	
-    default: async (event) => {		
-        let username = '';
-        let password = '';
-        let rememberMe = true;
-      
-            const url = 'http://localhost:8080/api/authenticate';
-            const data = {
-                username,
-                password,
-                rememberMe
-            };
-      
-            const response = await fetch(url, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(data)
-            });
-      
-            const result = await response.json();
-            // Gérer la réponse de l'API ici
-            console.log(result);
-            console.log("nn");
-            console.log(username)
 
-        }
-    };
