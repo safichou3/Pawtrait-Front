a9e612070b7c1eddd65a915f28c46d30de9c1d55
Safia <safia.medjahed@gmail.com>
2023-11-16 09:44:14 +0100

    feat: Style single image page

--- src/routes/galerie/[single]/+page.svelte
+++ src/routes/galerie/[single]/+page.svelte
@@ -2,12 +2,22 @@
     /** @type {import('./$types').PageData} */
     export let data;
 </script>
-<div class="back">Retour</div>
-<div class="flex justify-center gap-10">
-    <div class="left flex flex-col">
+
+<div>
+    <a href="../galerie" class="flex items-center gap-2 pl-5 pt-3 text-lg mb-5">
+        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...$$props}
+             class="text-3xl">
+            <path fill="currentColor"
+                  d="M11.707 5.293a1 1 0 0 1 0 1.414L7.414 11H19a1 1 0 1 1 0 2H7.414l4.293 4.293a1 1 0 0 1-1.414 1.414l-6-6a1 1 0 0 1 0-1.414l6-6a1 1 0 0 1 1.414 0z"/>
+        </svg>
+        Retour </a>
+</div>
+
+<div class="flex justify-center gap-10 max-md:flex-col-reverse">
+    <div class="left flex flex-col pl-3">
         <img src="{data.data[0].url}" alt="">
-        <p class="date text-right">23/10/2023</p>
-        <div class="tags text-center flex justify-around">
+        <p class="date mt-2 text-right">23/10/2023</p>
+        <div class="mt-2 tags text-center flex justify-around gap-2">
             <button class="border-2 px-4 py-1 bg-red-100 border-2 text-red-800 border-red-600 rounded-lg">Chats</button>
             <button class="border-2 px-4 py-1 bg-amber-100 border-2 text-amber-800 border-amber-600 rounded-lg">Chat
             </button>
@@ -23,14 +33,28 @@
         </div>
     </div>
     <div class="right flex flex-col">
-        <div class="user flex items-center gap-2">
-            <img src="https://via.placeholder.com/50" alt="Photo de profile">
+        <a href="../profil" class="user flex items-center gap-2 mb-5">
+            <img src="https://via.placeholder.com/70" alt="Photo de profile">
             <p class="text-lg">@MSafiax</p>
+        </a>
+        <p>Dans une large galerie d’images libre de droit, parcourez les créatures les plus mignonnes.</p>
+        <div class="flex items-end gap-2">
+            <svg xmlns="http://www.w3.org/2000/svg" class="cursor-pointer w-5 h-5 mt-5"
+                 role="presentation"
+                 data-name="Layer 1" style="fill:red" viewBox="0 0 24 24">
+                <path d="M17.5.917a6.4 6.4 0 0 0-5.5 3.3 6.4 6.4 0 0 0-5.5-3.3A6.8 6.8 0 0 0 0 7.967c0 6.775 10.956 14.6 11.422 14.932l.578.409.578-.409C13.044 22.569 24 14.742 24 7.967a6.8 6.8 0 0 0-6.5-7.05Z"/>
+            </svg>
+            150k
         </div>
-        <p>Dans une large galerie d’images libre de droit, parcourez les créatures les plus mignonnes</p>
-        <div class="params gap-2">
-            <button class="update border-2 bg-amber-300 border-amber-500 py-2 px-3 rounded-lg">Modifier</button>
-            <button class="delete bg-red-300 border-2 border-red-600 py-2 px-3 rounded-lg">Supprimer</button>
+        <div class="flex gap-5 pt-5">
+            <a href="../modify"
+               class="bg-yellow-400 py-2 px-3 rounded-lg hover:bg-yellow-500 duration-500 text-white font-semibold">
+                Modifier
+            </a>
+            <a href="../delete"
+               class="bg-red-500 py-2 px-3 rounded-lg hover:bg-red-600 duration-500 text-white font-semibold">
+                Supprimer
+            </a>
         </div>
     </div>
 </div>
