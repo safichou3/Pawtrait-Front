ff849dbebdf7101e25b0410309168d3bd8828e88
ousbahou <ousbane.houda@gmail.com>
2023-11-28 15:32:26 +0100

    affichage des photos

--- src/routes/galerie/+page.server.js
+++ src/routes/galerie/+page.server.js
@@ -1,11 +1,27 @@
 /** @type {import('./$types').PageServerLoad} */
-export async function load() {
-    const response = await fetch("https://api.thecatapi.com/v1/images/search?limit=30&api_key=live_c62EzoEgHNw9QRdu9JB4fU9JnZnBOpzV4SKpxaEo5iWNPAXIs7AM04Y4Vss31D6E");
-    // API KEY // live_c62EzoEgHNw9QRdu9JB4fU9JnZnBOpzV4SKpxaEo5iWNPAXIs7AM04Y4Vss31D6E
-    // console.log(response)
-    const data = await response.json();
-    // console.log(data)
-    return {
-        data
-    };
-};
+export async function load({cookies}) {
+    try {
+        // Récupérer le token d'accès de vos cookies ou de l'endroit approprié
+        const accessToken = cookies.get('sessionid');
+
+        const response = await fetch('http://localhost:8080/api/photos', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${accessToken}`, // Ajouter le token d'accès à l'en-tête
+            },
+            // Vous n'avez pas besoin du corps (body) pour une requête GET
+        });
+
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+
+            return {data}
+        } else {
+            console.error('Échec de la requête:', response.status, response.statusText, response.json);
+        }
+    } catch (error) {
+        console.error('Erreur lors de la requête:', error);
+    }
+}
