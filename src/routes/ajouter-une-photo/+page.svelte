7f1a72051cbad72f93be3525d353f12553460743
ousbahou <ousbane.houda@gmail.com>
2023-12-07 15:29:30 +0100

    ajout

--- src/routes/ajouter-une-photo/+page.svelte
+++ src/routes/ajouter-une-photo/+page.svelte
@@ -1,114 +1,205 @@
 <script>
-  /** @type {import('./$types').PageData} */
-  export let data;
+	/** @type {import('./$types').PageData} */
+	export let data;
 
-  function handleFileInputChange() {
-    const fileInput = document.getElementById('photoUrl');
-    console.log('Fichier sélectionné :', fileInput.files[0]);
-    updateFileName(fileInput.files[0]);
-  }
+	function handleFileInputChange() {
+		const fileInput = document.getElementById('photoUrl');
+		console.log('Fichier sélectionné :', fileInput.files[0]);
+		updateFileName(fileInput.files[0]);
+	}
 
-  function openFileInput() {
-    const fileInput = document.getElementById('photoUrl');
-    fileInput.click();
-  }
+	function openFileInput() {
+		const fileInput = document.getElementById('photoUrl');
+		fileInput.click();
+	}
 
-  function handleDragOver(event) {
-    event.preventDefault();
-    const dropArea = document.getElementById('drop-area');
-    dropArea.classList.add('drag-over');
-  }
+	function handleDragOver(event) {
+		event.preventDefault();
+		const dropArea = document.getElementById('drop-area');
+		dropArea.classList.add('drag-over');
+	}
 
-  function handleDragLeave(event) {
-    event.preventDefault();
-    const dropArea = document.getElementById('drop-area');
-    dropArea.classList.remove('drag-over');
-  }
+	function handleDragLeave(event) {
+		event.preventDefault();
+		const dropArea = document.getElementById('drop-area');
+		dropArea.classList.remove('drag-over');
+	}
 
-  function handleDrop(event) {
-    event.preventDefault();
-    const dropArea = document.getElementById('drop-area');
-    dropArea.classList.remove('drag-over');
+	function handleDrop(event) {
+		event.preventDefault();
+		const dropArea = document.getElementById('drop-area');
+		dropArea.classList.remove('drag-over');
 
-    const fileInput = document.getElementById('photoUrl');
-    const files = event.dataTransfer.files;
+		const fileInput = document.getElementById('photoUrl');
+		const files = event.dataTransfer.files;
 
-    if (files.length > 0) {
-      fileInput.files = files;
-      console.log('Fichier sélectionné :', fileInput.files[0]);
-      updateFileName(fileInput.files[0]);
-    }
-  }
+		if (files.length > 0) {
+			fileInput.files = files;
+			console.log('Fichier sélectionné :', fileInput.files[0]);
+			updateFileName(fileInput.files[0]);
+		}
+	}
 
-  function updateFileName(file) {
-    const fileNameDisplay = document.getElementById('file-name-display');
-    if (fileNameDisplay) {
-      fileNameDisplay.textContent = file ? file.name : '';
-    }
-  }
+	function updateFileName(file) {
+		const fileNameDisplay = document.getElementById('file-name-display');
+		const imagePreview = document.getElementById('image-preview');
+
+		if (fileNameDisplay && imagePreview) {
+			fileNameDisplay.textContent = file ? file.name : '';
+
+			// Show/hide image preview
+			if (file) {
+				imagePreview.classList.remove('hidden');
+			} else {
+				imagePreview.classList.add('hidden');
+			}
+
+			// Update image source
+			const reader = new FileReader();
+			reader.onload = function (e) {
+				imagePreview.src = e.target.result;
+			};
+			reader.readAsDataURL(file);
+		}
+	}
 </script>
 
-<form method="POST" action="?/sendImage" enctype="multipart/form-data">
+		<form
+			class="bg-white md:col-span-2"
+			method="POST"
+			action="?/sendImage"
+			enctype="multipart/form-data"
+		>
+			<div class="px-4 py-6 sm:p-8">
+				<div class="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+					<div class="col-span-full">
+						<label for="cover-photo" class="block text-sm font-medium leading-6 text-gray-900"  
+							>Ajouter une photo d'animal</label
+						>
+						<div class="col-span-full">
+							<div
+								class="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10"
+							>
+								<div class="text-center">
+									<div class="mt-4 flex text-sm leading-6 text-gray-600">
+										<!-- Zone de glisser-déposer -->
 
-  <div class="col-span-full">
-    <label for="cover-photo" class="block text-sm font-medium leading-6 text-gray-900">Photo de l'animal</label>
-    <div class="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
-      <div class="text-center">
-        <svg class="mx-auto h-12 w-12 text-gray-300" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
-          <path fill-rule="evenodd" d="M1.5 6a2.25 2.25 0 012.25-2.25h16.5A2.25 2.25 0 0122.5 6v12a2.25 2.25 0 01-2.25 2.25H3.75A2.25 2.25 0 011.5 18V6zM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0021 18v-1.94l-2.69-2.689a1.5 1.5 0 00-2.12 0l-.88.879.97.97a.75.75 0 11-1.06 1.06l-5.16-5.159a1.5 1.5 0 00-2.12 0L3 16.061zm10.125-7.81a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0z" clip-rule="evenodd" />
-        </svg>
-        <div class="mt-4 flex text-sm leading-6 text-gray-600">
-          <!-- Zone de glisser-déposer -->
-          <div id="drop-area" on:dragover={handleDragOver} on:dragenter={handleDragOver} on:dragleave={handleDragLeave} on:drop={handleDrop}>
-            <!-- Bouton personnalisé pour ouvrir la boîte de dialogue de téléchargement de fichier -->
-            <button type="button" id="customFileBtn" on:click={openFileInput}>
-              Ajoute une image
-            </button>
-            <input id="photoUrl" name="photoUrl" type="file" class="sr-only" on:change={handleFileInputChange}>
-            <p class="pl-1">ou glisse-la ici</p>
-            <p id="file-name-display" class="text-xs leading-5 text-gray-600"></p>
-          </div>
-        </div>
-      
-        <p class="text-xs leading-5 text-gray-600">PNG, JPG, GIF up to 10MB</p>
-      </div>
-    </div>
-  </div>
+										<div
+											id="drop-area"
+											on:dragover={handleDragOver}
+											on:dragenter={handleDragOver}
+											on:dragleave={handleDragLeave}
+											on:drop={handleDrop}
+										>
+											<!-- Bouton personnalisé pour ouvrir la boîte de dialogue de téléchargement de fichier -->
+											<button type="button" id="customFileBtn" on:click={openFileInput}>
+												Ajoute une image
+											</button>
+											<input
+												id="photoUrl"
+												name="photoUrl"
+												type="file"
+												class="sr-only"
+												on:change={handleFileInputChange}
+											/>
+											<p class="pl-1">ou glisse-la ici</p>
+											<p id="file-name-display" class="text-xs leading-5 text-gray-600" />
+                      <img id="image-preview" src="" alt="Image Preview" class="hidden max-w-full mt-4">
 
-	<label for="description">Description:</label>
-	<input type="text" id="description" name="description" required /><br />
+										</div>
+									</div>
 
-	<label for="categoryId">Category:</label>
-	<select id="categoryId" name="categoryId" required>
-		{#if data.categories}
-			{#each data.categories as category (category.id)}
-				<option value={category.id}>{category.name}</option>
-			{/each}
-		{/if}
-	</select><br />
+									<p class="text-xs leading-5 text-gray-600">Formats acceptés PNG, JPG, GIF</p>
+								</div>
+							</div>
+						</div>
+					</div>
 
-	<button type="submit">Envoyer la photo</button>
-</form>
+					<div class="sm:col-span-4">
+						<label for="categoryId" class="block text-sm font-medium leading-6 text-gray-900"
+							>Catégorie</label
+						>
+						<div class="mt-2">
+							<select
+								id="categoryId"
+								name="categoryId"
+								class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+							>
+								{#if data.categories}
+									{#each data.categories as category (category.id)}
+										<option value={category.id}>{category.name}</option>
+									{/each}
+								{/if}
+							</select>
+						</div>
+					</div>
+
+					<div class="col-span-full">
+						<label for="about" class="block text-sm font-medium leading-6 text-gray-900"
+							>Description</label
+						>
+						<p class="mt-3 text-sm leading-6 text-gray-600">
+							Décris la photo en détail (exemple: Ceci est un lapin qui joue aux cartes).
+						</p>
+						<div class="mt-2">
+							<textarea
+								id="description"
+								name="description"
+								rows="3"
+								class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+							/>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div
+				class="flex items-center justify-end gap-x-6 border-t border-gray-900/10 px-4 py-4 sm:px-8"
+			>
+				<button type="button" class="text-sm font-semibold leading-6 text-gray-900">Retour</button>
+				<button
+					type="submit"
+					class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+					>Envoyer la photo</button
+				>
+			</div>
+		</form>
+
 
 <style>
-  #customFileBtn {
-    /* Ajoutez vos styles personnalisés ici */
-    cursor: pointer;
-    color: blue;
-  }
+	#customFileBtn {
+		/* Ajoutez vos styles personnalisés ici */
+		cursor: pointer;
+		color: blue;
+	}
 
-  #drop-area {
-    border: 2px dashed #ddd;
-    padding: 20px;
-    border-radius: 5px;
-  }
+	#drop-area {
+		border: 2px dashed #ddd;
+		padding: 20px;
+		border-radius: 5px;
+	}
 
-  #drop-area.drag-over {
-    background-color: #f0f0f0;
-  }
+	#drop-area.drag-over {
+		background-color: #f0f0f0;
+	}
 
-  p#file-name-display {
-    background-color: #a1faa1;
-    border-radius: 27px;
-}
+	p#file-name-display {
+		background-color: #a1faa1;
+		border-radius: 27px;
+	}
+	.px-4.py-6 .sm\:p-8 .s-KtQKLbXmKp_P {
+		margin: auto;
+	}
+	#image-preview {
+		max-width: 100%;
+		height: auto;
+		border-radius: 5px;
+	}
+
+	.hidden {
+		display: none;
+	}
+
+  #image-preview{
+    border-radius: 20px;
+  }
 </style>
