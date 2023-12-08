137088f4b065f3ca0b4ba529eb1969d759e3dccb
AntoineBrevet <antoine.brevet@live.fr>
2023-12-08 09:37:23 +0100

    is logged in reparation

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -6,17 +6,8 @@
   
   let menuOpen = false;
   let routeID = $page.route.id;
-  let isLoggedIn = false; // Initially assume the user is not logged in
 
   $: if ($navigating) routeID = $page.route.id;
-
-  // Check if "userid" cookie is present on component mount
-  onMount(() => {
-    const cookies = document.cookie;
-    console.log(cookies)
-    const userIdCookie = cookies.split(';').find(cookie => cookie.trim().startsWith('userid='));
-    isLoggedIn = !!userIdCookie;
-  });
 </script>
 
 
@@ -39,7 +30,6 @@
             <a href="/classement"   class={routeID === '/classement' ? 'border-indigo-500 text-gray-900 inline-flex items-center border-b-2 px-1 pt-1 text-sm font-medium' : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center border-b-2 px-1 pt-1 text-sm font-medium'}>
              PHOTO DE LA SEMAINE
             </a>
-            {#if isLoggedIn}
             <a href="/ajouter-une-photo"   class={routeID === '/ajouter-une-photo' ? 'text-gray-900 inline-flex items-center px-1 pt-1 text-sm font-medium' : 'text-gray-500 hover:text-gray-700 inline-flex items-center px-1 pt-1 text-sm font-medium'}>
 
               <button type="button" class="inline-flex items-center gap-x-1.5 rounded-md bg-indigo-600 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm ">
@@ -49,7 +39,6 @@
                 Ajouter une photo
               </button>          
             </a>
-            {/if}
           </div>
 
         </div>
